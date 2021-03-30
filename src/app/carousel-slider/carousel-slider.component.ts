a9665e68df6cea872958779a00c075672fb6cca3
Mohit Kapoor <mkapoor@KENTMAIL.GRP>
2021-03-30 12:37:14 +0530

    first commit

---
+++ src/app/carousel-slider/carousel-slider.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { Slide } from "./slide.interface";
+
+@Component({
+  selector: 'app-carousel-slider',
+  templateUrl: './carousel-slider.component.html',
+  styleUrls: ['./carousel-slider.component.scss']
+})
+export class CarouselSliderComponent implements OnInit {
+
+  slideConfig: any;
+  slides: Slide[] = [
+    {
+      title: `Mobile internet`
+    },
+    {
+      title: `Home internet`
+    },
+    {
+      title: `Get a device`
+    },
+    {
+      title: `Add a phone-line`
+    },
+    {
+      title: `Upgrade`
+    }
+  ];
+  currnetSlideIndex: number = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+   this.slideConfig = {
+     centerMode: true,
+     slidesToShow: 4.39,
+     speed: 500,
+     arrows: false,
+     draggable: true,
+     dots: false,
+     swipeToSlide: true,
+     accessibility: false,
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           arrows: false,
+           centerMode: true,
+           slidesToShow: 3,
+           speed: 500,
+           accessibility: false,
+         }
+       },
+       {
+         breakpoint: 768,
+         settings: {
+           arrows: false,
+           centerMode: true,
+           slidesToShow: 1,
+           speed: 500,
+           accessibility: false,
+         }
+       },
+       {
+         breakpoint: 400,
+         settings: {
+           arrows: false,
+           centerMode: true,
+           slidesToShow: 1,
+           speed: 500,
+           accessibility: false,
+         }
+       }
+     ]
+   }
+  }
+
+  /*
+  * Track current slide index
+  * */
+  beforeChangeEvent(event) {
+    this.currnetSlideIndex = event.nextSlide;
+  }
+
+}
